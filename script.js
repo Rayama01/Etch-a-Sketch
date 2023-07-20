23d91a0bc104166d4979a25319abf8d60dd11707
Rayama01 <125508519+Rayama01@users.noreply.github.com>
2023-07-20 14:06:05 -0500

    Added 'etch a sketch' heading

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const contain = document.querySelector(".container");
+const newGridBtn = document.querySelector(".btnDiv")
 
 for(i = 0; i < 16 * 16; i++) {
     const grid = document.createElement("div");
     grid.classList = 'grid-cell'
     contain.appendChild(grid);
 }
+
