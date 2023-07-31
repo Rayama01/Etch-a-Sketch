972e958f354cdf554f232a879e181f6a407134ca
Rayama01 <125508519+Rayama01@users.noreply.github.com>
2023-07-31 14:38:36 -0500

    Removed buttons and replaced with new grid button and default button and made function that on click prompts user to input width and height then a new grid gets generated

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 const contain = document.querySelector(".container");
-const newGridBtn = document.querySelector(".btnDiv")
+let defaultBtn = document.querySelector(".defaultBtn");
+const newGridBtn = document.querySelector(".newGridBtn");
+let grid;
 
-for(i = 0; i < 16 * 16; i++) {
-    const grid = document.createElement("div");
-    grid.classList = 'grid-cell'
-    contain.appendChild(grid);
+function grid16() {
+            for(i = 0; i < 16 * 16; i++) {
+                grid = document.createElement("div");
+                grid.classList = "grid-cell";
+                grid.setAttribute('style', 'width: 34.375px; height: 34.375px; box-sizing: border-box; border: 1px solid black;');
+                contain.appendChild(grid);
+            }
+    }
+
+grid16();
+
+
+
+
+newGridBtn.onclick = function() {
+    contain.innerHTML = "";
+    gridWidth = parseInt(prompt("Enter new width less than 100: "));
+    gridHeight = parseInt(prompt("Enter new height less than 100: "));
+    if(gridWidth <= 100 & gridHeight <= 100){
+        for(i = 0; i < gridWidth * gridHeight; i++) {
+            grid = document.createElement("div");
+            grid.classList = "grid-cell";
+            grid.setAttribute('style','box-sizing: border-box; border: 1px solid black;');
+            grid.style.width = 550 / gridWidth + "px";
+            grid.style.height = 550 / gridHeight + "px";
+            contain.appendChild(grid);
+        }
+    } else {
+        grid16();
+    }
+    defaultBtn.onclick = function() {
+        contain.innerHTML = "";
+        grid16();
+    }
+    
 }
-
